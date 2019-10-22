c9edcec5a06a5def05500d433a7d88fb7a276ef0
Michel Anderson Lütz Teixeira <michel_lutz@icloud.com>
2019-10-22 08:41:12 -0300

    Initial Commit

---
+++ SwiftUI Components Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Components Demo
+//
+//  Created by Michel Teixeira on 22/10/19.
+//  Copyright © 2019 Michel Teixeira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
